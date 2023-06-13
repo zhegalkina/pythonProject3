d6eda6ba9f23669d5e0960f91bf0e9b5ca5cbde3
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-13 21:32:34 +0300

    tasks

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-
-new_list = [x for x in range(1,len(a)) if a[x] > a[x-1]]
-print(a(x))
+new_list_1 = [int(i) for i in input().split()]
+new_list = [i for i in range(1,len(new_list_1)) if new_list_1[i] > new_list_1[i-1]]
+print(new_list(i))
