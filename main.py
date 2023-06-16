1635cea2a105c683ff56440461f9298536ce3433
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-16 19:16:11 +0300

    tasks

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# new_list_1 = [int(i) for i in input().split()]
-# new_list = [new_list_1[i] for i in range(1,len(new_list_1)) if new_list_1[i] > new_list_1[i-1]]
-# print(new_list)
-
-
-new_list_1 = [int(i) for i in input().split()]
-print(max(new_list_1))
-print(new_list_1.index(max(new_list_1)))
+d ={}
+d = {'x': 10}
+for key, value in d.items():
+    print(key, '->', value)
