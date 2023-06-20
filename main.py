39390669fd424548e8eabd22da46fcf34cd00e69
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-20 19:38:27 +0300

    tasks

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-deg = int(input("Введите температуру: "))
-keyword = input("в: ")
-def function(deg, keyword):
-    if keyword == 'c':
-        return deg*1.8+32
-    elif keyword == 'f':
-        return (deg-32)/1.8
-print(function(deg, type))
+digit = int(input("Число: "))
+if digit % 4 ==0 and digit % 100 != 0 or digit % 400 == 0:
+    print("Високосный")
+else:
+    print("нет")
