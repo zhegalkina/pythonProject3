25f763022fb67f775a7d561ae1ed6b8aac42d338
Zhegalkina <tanya.zhegalkina@mail.ru>
2023-06-27 22:04:28 +0300

    tasks

--- program.py
+++ program.py
@@ -1,15 +1,16 @@
-topic = input("What is my name?")
 answer = input("Enter your answer: ")
 correct_answer = input("Enter the correct one: ")
 if answer == correct_answer:
     print("you are the winner")
 if answer != correct_answer:
     print("2 attempts left")
-    pass
 answer = input("Enter your answer: ")
 if answer != correct_answer:
     print("1 attempt left")
-    pass
+if answer == correct_answer:
+    print("you are the winner")
 answer = input("Enter your answer: ")
 if answer != correct_answer:
     print("failed")
+if answer == correct_answer:
+    print("you are the winner")
